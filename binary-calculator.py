cdac3b4e06c829aa1d66173ecc7d106d681d1d9c
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-05-03 18:37:59 -0700

    Update binary-calculator.py

--- binary-calculator.py
+++ binary-calculator.py
@@ -35,16 +35,20 @@ with col3:
             is_binary = 1
             for x in list(str(binary)):
                 st.write(x)
+                st.write(f"in for loop:{is_binary}")
                 if x == "0" or "1":
                     continue
                 else:
                     st.write("Please enter a valid binary number...")
                     is_binary = 0
+                    st.write(f"if else condition:{is_binary}")
                     break
+            st.write(f"outside for loop: {is_binary}")
             if is_binary == 1:
                 num = int(str(binary), 2)
                 st.write(f"{num}₁₀")
             elif is_binary == 0:
+                st.write(f"elseif condition: {is_binary}")
                 st.write("Please enter a valid binary number...")
     elif convertor == "Decimal to Binary":
         num = convertd(decimal)
